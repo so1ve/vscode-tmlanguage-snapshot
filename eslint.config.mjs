cd85434c12ef26a5b9aa655790ed5d9349c228f1
so1ve <58381667+so1ve@users.noreply.github.com>
2026-07-11 01:57:04 +0800

    test: move fixtures

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,6 +3,6 @@ import { so1ve } from "@so1ve/eslint-config";
 export default so1ve({
   ignores: [
     "packages/vscode/playground/fixtures/**",
-    "test/__fixtures__/vue-language-tools/**",
+    "packages/standalone/test/__fixtures__/vue-language-tools/**",
   ],
 });
